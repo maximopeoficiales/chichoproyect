b8c8227a90b42b9963ffa11b3ebe0a43b627e8ad
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-08-03 22:42:25 -0500

    falta consumir api y pasar a react

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-let edad = new Date();
-edad = edad.getFullYear() - 2001;
+let ano = new Date().getFullYear();
+let edad = ano - 2001;
 document.querySelector("#edad").innerHTML = edad;
+document.querySelector("#ano").innerHTML = ano;
