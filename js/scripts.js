6aba67a3a59966a0dcdf8a161400e4628d011ab5
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-08-07 02:39:52 -0500

    animaciones agregadas

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,44 @@ let ano = new Date().getFullYear();
 let edad = ano - 2001;
 document.querySelector("#edad").innerHTML = edad;
 document.querySelector("#ano").innerHTML = ano;
+
+window.sr = ScrollReveal();
+sr.reveal("#datos_principales", {
+  duration: 1000,
+  origin: "top",
+  distance: "300px" /* distancia desde arriba en este caso */,
+  reset: true,
+});
+sr.reveal("#algunas_palabras", {
+  duration: 1000,
+  origin: "left",
+  distance: "100px" /* distancia desde arriba en este caso */,
+});
+sr.reveal("#breve_descripcion", {
+  duration: 1000,
+  origin: "left",
+  distance: "100px" /* distancia desde arriba en este caso */,
+});
+sr.reveal("#mis_habilidades", {
+  duration: 1000,
+  origin: "right",
+  distance: "100px" /* distancia desde arriba en este caso */,
+});
+
+sr.reveal(".card-proyect", {
+  duration: 1000,
+  origin: "left",
+  distance: "100px" /* distancia desde arriba en este caso */,
+});
+
+sr.reveal(".card-custom", {
+  duration: 1000,
+  origin: "left",
+  distance: "100px" /* distancia desde arriba en este caso */,
+});
+
+sr.reveal(".contacto_m", {
+  duration: 1000,
+  origin: "left",
+  distance: "100px" /* distancia desde arriba en este caso */,
+});
